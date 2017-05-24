f14218d112e05a3ee3a9af170f815d3220c5638f
Dmitriev_Svuatoslav <emerld@mail.ru>
2017-05-24 19:24:59 +0300

    Memory leak fix

--- AtomGame/main.cpp
+++ AtomGame/main.cpp
@@ -15,15 +15,16 @@ int main (int argc, char **argv)
 {
     std::string initFileName = "log4cpp.properties";
     log4cpp::PropertyConfigurator::configure (initFileName);
-
-    std::vector<Plugin *> plugins;
+    std::vector<std::pair<void *, Plugin *>> plugins;
     for (int i = 1; i < argc; i++)
     {
         const char *so = argv[i];
         if (void *handle = dlopen (argv[i], RTLD_LAZY))
         {
             if (Plugin *(*create) () = (Plugin *(*) ()) dlsym (handle, "create"))
-                plugins.push_back (create ());
+                plugins.push_back (std::pair<void *, Plugin *> (handle, create ()));
+            else
+                dlclose (handle);
         } else
             std::cout << dlerror () << '\n';
 
@@ -35,15 +36,21 @@ int main (int argc, char **argv)
     View view (&model, 600, 400);
     Controller controller (&model, &view);
 
-    for (std::vector<Plugin *>::iterator i = plugins.begin (); i != plugins.end (); i++)
-        (*i)->start (&model, &controller, &view);
+    for (std::vector<std::pair<void *, Plugin *>>::iterator i = plugins.begin (); i != plugins.end (); i++)
+        i->second->start (&model, &controller, &view);
 
     while (!controller.isEnd ())
     {
         controller.tick ();
         model.tick ();
-        if (!view.tick ()) return 0;
-        for (std::vector<Plugin *>::iterator i = plugins.begin (); i != plugins.end (); i++)
-            (*i)->tick ();
+        if (!view.tick ()) break;
+        for (std::vector<std::pair<void *, Plugin *>>::iterator i = plugins.begin (); i != plugins.end (); i++)
+            i->second->tick ();
+    }
+
+    for (std::vector<std::pair<void *, Plugin *>>::const_iterator i = plugins.cbegin (); i != plugins.cend (); i++)
+    {
+        delete i->second;
+        dlclose (i->first);
     }
 }
